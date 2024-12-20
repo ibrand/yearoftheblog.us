ff9294e5a5d698c6ed0e25bc2c4b1eeb926fb958
Ilona Brand <theilonabrand@gmail.com>
2024-12-20 15:34:08 -0500

    big spinner rewrite to account for many blogs in orbit

--- public/script.js
+++ public/script.js
@@ -1,6 +1,23 @@
-let blogsListings = document.getElementsByClassName("listing")
+const blogsListings = document.getElementsByClassName("listing")
+const orbitRings = document.getElementsByClassName("ring")
+const list = document.getElementById("list")
+const DELAY_BETWEEN_LISTINGS_IN_SECONDS = 15;
 
-// Items should not do any funky click animations
+let counter = 0;
 Array.from(blogsListings).forEach(blogListing => {
     blogListing.addEventListener("click", (e) => { e.stopPropagation() });
 });
+
+Array.from(orbitRings).forEach(ring =>  {
+    prepOrbits(ring, counter)
+    counter = counter+DELAY_BETWEEN_LISTINGS_IN_SECONDS;
+})
+
+function prepOrbits(ring, counter) {
+    ring.style.setProperty('animation-delay', `${counter}s`)
+}
+
+list.addEventListener("click", (e) => {
+    list.classList.toggle("spin");
+    list.classList.toggle("list");
+})
